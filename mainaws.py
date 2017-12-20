f2c74a7d4ef5932ddb267cdc14f1a8d8fbeb1761
Jackson Seven Delta <jacksonsevendelta@gmail.com>
2017-12-20 17:11:52 +1100

    Update mainaws.py

--- mainaws.py
+++ mainaws.py
@@ -1,5 +1,5 @@
 import requests
 
-text = input("Sample Bot Text: ")
-
-r = requests.post('https://hooks.slack.com/services/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', json={"text": text})
+while True:
+  text = input("Sample Bot Text: ")
+  r = requests.post('https://hooks.slack.com/services/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', json={"text": text})
